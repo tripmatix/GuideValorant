deeb67eda8e763fad616daba1eb16a24418f0910
Romulo Oliveira Viganico <romuloviganico@icloud.com>
2020-08-10 14:53:25 -0700

    Initial Commit

---
+++ GuideValorant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuideValorant
+//
+//  Created by Romulo Oliveira Viganico on 10/08/20.
+//  Copyright © 2020 Romulo Oliveira Viganico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GuideValorantTests/GuideValorantTests.swift
@@ -0,0 +1,34 @@
+//
+//  GuideValorantTests.swift
+//  GuideValorantTests
+//
+//  Created by Romulo Oliveira Viganico on 10/08/20.
+//  Copyright © 2020 Romulo Oliveira Viganico. All rights reserved.
+//
+
+import XCTest
+@testable import GuideValorant
+
+class GuideValorantTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
